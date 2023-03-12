1dc8eec2f9d6569dbe79364320e08c2ce2833839
albanodirosa <albanodirosa90@gmail.com>
2023-03-12 18:37:45 -0300

    Problem solved

--- homework/homework.js
+++ homework/homework.js
@@ -1,1 +1,32 @@
 //Escriba aquí su código JavaScript:
+function generar(){
+    return Math.ceil(Math.random() * 10);
+}
+
+let nro = generar();
+
+function comparar(){
+    let nroUsuario = document.getElementById("nroUsuario_id").value;
+    if (nroUsuario < nro) {
+        alert("El número es mayor que el ingresado");
+    } else if (nroUsuario > nro) {
+        alert("El número es menor que el ingresado");
+    } else {
+        alert("Felicitaciones! Adivinaste!!!");
+    }
+}
+
+let intentos = 0;
+
+function contador(){
+    return intentos += 1;
+}
+
+function jugar(){
+    intentos += 1;
+    if (intentos < 3) {
+    comparar();
+    } else {
+        alert("Has agotado los 3 intentos")
+    }    
+}
